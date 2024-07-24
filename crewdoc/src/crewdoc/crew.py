41bd447a203760da17336b3e8c5e13f9c6616513
Joaquin <angiotensina@icloud.com>
2024-07-24 22:12:32 +0200

    hierarchical2

--- crewdoc/src/crewdoc/crew.py
+++ crewdoc/src/crewdoc/crew.py
@@ -7,6 +7,17 @@ from crewdoc.tools.custom_tool import RAG_OpenAI
 # Check our tools documentations for more information on how to use them
 from crewai_tools import SerperDevTool
 
+import os
+from dotenv import load_dotenv, find_dotenv
+from langchain_openai import ChatOpenAI
+
+# Carga las variables de entorno desde un archivo .env
+load_dotenv(find_dotenv(), override=True)
+
+# Obtiene la API key de OpenAI desde las variables de entorno
+api_key_openAI = os.environ.get("OPENAI_API_KEY")
+model = os.environ.get("OPENAI_MODEL_NAME")
+
 @CrewBase
 class CrewdocCrew():
 	"""Crewdoc crew"""
@@ -19,6 +30,7 @@ class CrewdocCrew():
 			config=self.agents_config['researcher'], # Debe apuntar al decorador correspondiente del archivo agents.yaml
 			#tools=[], # Example of custom tool, loaded on the beginning of file
 			verbose=True,
+			cache = True,
 			allow_delegation=True
 		)
 
@@ -28,7 +40,9 @@ class CrewdocCrew():
 			config=self.agents_config['reporting_analyst'],	# Debe apuntar al decorador correspondiente del archivo agents.yaml
    			#tools=[], # Example of custom tool, loaded on the beginning of file
 			verbose=True,
-			allow_delegation=True
+			cache = True,
+			allow_delegation=True,
+   			max_iter = 3,
 		)
   
 	@agent
@@ -37,7 +51,9 @@ class CrewdocCrew():
 			config=self.agents_config['endocrinology_agent'],	# Debe apuntar al decorador correspondiente del archivo agents.yaml
    			tools=[RAG_OpenAI()], # Example of custom tool, loaded on the beginning of file
 			verbose=True,
-			allow_delegation=True
+			cache = True, 
+			allow_delegation=True,
+   			max_iter = 3,				
 		)
   
 	@agent
@@ -46,8 +62,23 @@ class CrewdocCrew():
 			config=self.agents_config['reumatology_agent'],	# Debe apuntar al decorador correspondiente del archivo agents.yaml
    			tools=[RAG_OpenAI()], # Example of custom tool, loaded on the beginning of file
 			verbose=True,
-			allow_delegation=True
+			cache = True,
+			allow_delegation=True,
+	   		max_iter = 3,			
 		)
+
+	# Define the manager agent
+	@agent
+	def manager(self) -> Agent:
+		return Agent(
+			role="Project Manager",
+			goal="Efficiently manage the crew and ensure high-quality task completion",
+			backstory="You're an experienced project manager, skilled in overseeing complex projects and guiding teams to success. Your role is to coordinate the efforts of the crew members, ensuring that each task is completed on time and to the highest standard.",
+			allow_delegation=True,
+			verbose=True,
+		)
+
+	# Establishing the crew with a hierarchical process and additional configurations
 
 	@task
 	def clinical_analysis_task(self) -> Task:
@@ -84,7 +115,11 @@ class CrewdocCrew():
 		return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=2,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+			manager_llm=ChatOpenAI(temperature=0, model = model), # Mandatory if manager_agent is not set. Mandatory in hierarchical process
+			memory= True, # Enable memory usage for enhanced task execution
+   			verbose=2,
+      		#process=Process.sequential,
+			#manager_agent=None,  # Optional: explicitly set a specific agent as manager instead of the manager_llm
+			manager_agent=self.manager, # Optional: explicitly set a specific agent as manager instead of the manager_llm
+   			process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
